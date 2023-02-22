6028fe1d57d36ec7a5ad206d1c4a176472c22c19
SanjatulHasanSiam <sanjatulhasansiam.iit@gmail.com>
2023-02-22 22:37:13 +0600

    Product Management

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -11,14 +11,17 @@ namespace BulkyBook.DataAccess.Repository
     public class UnitOfWork : IUnitOfWork
     {
         private ApplicationDbContext _db;
+        public ICategoryRepository Category { get; private set; }
+        public ICoverTypeRepository CoverType { get; private set; }
+        public IProductRepository Product { get; private set; }
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
-            CoverType= new CoverTypeRepository(_db);    
+            CoverType= new CoverTypeRepository(_db);   
+            Product=new ProductRepository(_db);
         }
-        public ICategoryRepository Category { get; private set; }
-        public ICoverTypeRepository CoverType { get; private set; }
+        
         public void Save()
         {
             _db.SaveChanges();
